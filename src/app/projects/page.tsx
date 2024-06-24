d81d9874c3ab8f87bcc3118192522986b77cedfb
unknown <gabriel.alejandro.paparello@gmail.com>
2024-06-24 05:10:18 -0300

    sdaa555s

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,15 @@
+
+import React from 'react'
+
+const page = () => {
+  return (
+    <main>
+          <div>
+              <h1>Create Project</h1>
+              <button className="bg-blue-500 mt-10 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"><a href="">Start Project</a></button>
+          </div>
+    </main>
+  )
+}
+
+export default page
