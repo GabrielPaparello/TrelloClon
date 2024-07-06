baa0fd271392a5d5d4be1094f602ec63711d20b4
unknown <gabriel.alejandro.paparello@gmail.com>
2024-07-06 16:15:11 -0300

    sad

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,11 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="">
-        <h1>Trello Clone</h1>
-        
-        <button className="bg-blue-500 mt-10 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"><Link href="projects">Get Started</Link></button>
+    <main className="flex min-h-screen  items-center justify-center   p-24">
+      <div className="flex flex-col justify-center content-center align-middle text-center items-center">
+        <h1 className="text-7xl">Trello Clone</h1>
+        <h3>Start managing the work flow of your team</h3>
+        <button className="w-[200px] bg-blue-500 mt-10 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"><Link href="projects">Get Started</Link></button>
       </div>
     </main>
   );
