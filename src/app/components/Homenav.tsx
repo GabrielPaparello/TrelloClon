a7e1ef8f4453ebdc2e03e5860225731348273211
GabrielPaparello <69154223+GabrielPaparello@users.noreply.github.com>
2024-07-11 09:14:32 -0300

    img? nav?

--- src/app/components/Homenav.tsx
+++ src/app/components/Homenav.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 const Homenav = () => {
   return (
         <nav>
-            <Image src='./assets/logo.png' alt='logoBrand'/>
+            <Image src='/assets/logo.png' alt='logoBrand' width={500} height={300}/>
             <h2>BoardStack</h2>
         </nav>
 
