68c9ab607150bfd19c1451bc2cc1edd1d8e85a9e
GabrielPaparello <69154223+GabrielPaparello@users.noreply.github.com>
2024-07-11 10:36:16 -0300

    font2

--- src/app/components/Homenav.tsx
+++ src/app/components/Homenav.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Image from 'next/image'
+import "./globals.css";
 
 const Homenav = () => {
   return (
         <nav className="flex ">
           <aside>
             <Image src='/assets/logo.png' alt='logoBrand' width={59} height={59}/>
-            <h2 className={`${raleway.className}`}>BoardStack</h2>
+            <h2 className={`raleway`}>BoardStack</h2>
           </aside>
           <ul>
           <li>
