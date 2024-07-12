948205bb0949605ddfbcc2f01a1d59a207a6b173
GabrielPaparello <69154223+GabrielPaparello@users.noreply.github.com>
2024-07-12 10:45:10 -0300

    lastcommit

--- src/app/lib/assets/features.ts
+++ src/app/lib/assets/features.ts
@@ -7,28 +7,28 @@ export const featuresImg = [
         id: 'feature1'
     },
     {
-        imgPath: '/assets/feature/f2.jpeg',
+        imgPath: '/assets/feature/f2.png',
         h2: 'Advanced Task Management',
         h3: 'Stay on top of tasks with advanced management features.',
         imgAlt:'Image of feature 2',
         id:'feature2'
     },
     {
-        imgPath: '/assets/feature/f3.jpeg',
+        imgPath: '/assets/feature/f3.png',
         h2: 'Intuitive Interface',
         h3: 'Drag-and-drop functionality, customizable boards and easy to nativage layouts',
         imgAlt:'feature3',
         id:'feature3'
     },
     {
-        imgPath: '/assets/feature/f4.jpeg',
+        imgPath: '/assets/feature/f4.png',
         h2: 'Customizable Workflows',
         h3: 'Custom fields, labels, and board templates that can be adjusted to suit diferent project needs',
         imgAlt:'feature4',
         id:'feature4'
     },
     {
-        imgPath: '/assets/feature/f5.jpeg',
+        imgPath: '/assets/feature/f5.png',
         h2: 'Seamless Collaboration',
         h3: 'Shared boards, comments, and @mentions to keep everyone in the loop.',
         imgAlt:'feature5',
