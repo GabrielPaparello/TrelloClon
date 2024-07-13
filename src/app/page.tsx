c9102ddb3a72e830a98f5d091b94d4abdd821599
GabrielPaparello <69154223+GabrielPaparello@users.noreply.github.com>
2024-07-13 08:28:30 -0300

    carouselbug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,13 +9,24 @@ import "react-multi-carousel/lib/styles.css";
 
 export default function Home() {
   const [slideIndex, setSlideIndex] = useState(0);
-   useEffect(() => {
-        const interval = setInterval(() => {
-            setSlideIndex(prevIndex => (prevIndex + 1) % features.length);
-        }, 3000); // Adjust slide duration (milliseconds)
-
-        return () => clearInterval(interval);
-    }, [features.length]);
+  const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 3,
+    slidesToSlide: 3 // optional, default to 1.
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2,
+    slidesToSlide: 2 // optional, default to 1.
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+    slidesToSlide: 1 // optional, default to 1.
+  }
+};
+    
   return (
     <>
       <header>
@@ -59,7 +70,7 @@ export default function Home() {
   swipeable={false}
   draggable={false}
   showDots={true}
-  // responsive={responsive}
+  responsive={responsive}
   ssr={true} // means to render carousel on server-side.
   infinite={true}
   autoPlay
