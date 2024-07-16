7c8da17b1bfee1cb2339d5fc7bfe753b923ab3e1
GabrielPaparello <69154223+GabrielPaparello@users.noreply.github.com>
2024-07-16 09:58:16 -0300

    butons on hero

--- src/app/components/Sections/Hero.tsx
+++ src/app/components/Sections/Hero.tsx
@@ -14,7 +14,7 @@ const Hero = () => {
           Streamline your projects and collaborate seamlessly with our intuitive
           task management platform.
         </h3>
-        <div className="space-y-4">
+        <div className="space-y-4 space-x-4 mt-[50px]">
           <Link href="projects">
             <a className="w-52 bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded inline-block">
               Get Started
