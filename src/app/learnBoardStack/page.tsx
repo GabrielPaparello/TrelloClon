7d4a79bb41ff924fc5711bd5edd3630543e7ceaa
gabrielpaparello <69154223+GabrielPaparello@users.noreply.github.com>
2024-07-17 13:24:41 +0000

    LearnBoard

--- src/app/learnBoardStack/page.tsx
+++ src/app/learnBoardStack/page.tsx
@@ -2,6 +2,9 @@
 import React from "react";
 import Image from "next/image";
 import Homenav from "../components/Homenav";
+import "../globals.css";
+import AndroidIcon from "@mui/icons-material/Android";
+import AppleIcon from "@mui/icons-material/Apple";
 
 const page = () => {
   return (
@@ -18,7 +21,7 @@ const page = () => {
         <section className="flex flex-col p-5 gap-10 fira ">
           {/*video explanation */}
           <h2 className="text-xl raleway">What is a Kanban Board</h2>
-          <p className="fira">
+          <p className=" fira ">
             We got to start like everything with the basics. A Kanban board is
             an agile project management tool designed to help visualize work,
             limit work in progress, and maximize efficiency (or flow). It can
@@ -27,26 +30,44 @@ const page = () => {
           </p>
           <Image
             src="/assets/blog/kanbanExample.png"
-            width={500}
-            height={411}
+            width={400}
+            height={311}
             alt="kanBan elements example"
-            className=""
+            className="self-center ring-[#6ee2f5]/50 shadow-lg shadow-[#6ee2f5] ring-1 ring-offset-2 ring-offset-current hover:scale-[1.35] transition-all duration-300 "
           />
         </section>
-        <section>
-          <h2>What BoardStack offers</h2>
-          <ol>
+        <section className="flex flex-col gap-10 mt-20 p-5">
+          <h2 className="raleway text-xl">What BoardStack offers</h2>
+          <ol className="list-disc p-2">
             <li>Create Cards</li>
-            <li></li>
+            <li>Organize Tasks in each card</li>
+            <li>Details of task and positibility to add checklists</li>
+            <li>
+              <span className="text-gray-500 ">(currently developing)</span>{" "}
+              other types of files for details
+            </li>
+            <li>
+              <span className="text-gray-500">(currently developing)</span> re
+              arrenge tasks beetween cards
+            </li>
           </ol>
           <p>
             Our app includes a login feature with convenient options like
-            Google, or you can register manually. Currently, it's designed for
+            Google, or you can register manually. Currently, its designed for
             managing a single project at a time, as it serves primarily as a
-            portfolio piece. However, you can save and reload your current
-            project whenever needed. Currently available on the web, we are
-            actively developing the mobile app for Android and iOS platforms.
+            portfolio piece.
           </p>
+          <Image src="" alt="img" />
+          <br></br>
+          <p>
+            However, you can save and reload your current project whenever
+            needed. Currently available on the web, we are actively developing
+            the mobile app for Android and iOS platforms.
+          </p>
+          <aside className="flex  justify-center -mt-5 space-x-5  ">
+            <AndroidIcon className="text-5xl hover:text-[#3DDC84] transition-all duration-300" />
+            <AppleIcon className="text-5xl  stroke-black text-white  hover:text-black hover:stroke-white" />
+          </aside>
         </section>
       </main>
     </>
