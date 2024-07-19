528de30b52e62d049c88f5f0567358e39a0243db
gabrielpaparello <69154223+GabrielPaparello@users.noreply.github.com>
2024-07-19 11:49:17 +0000

    Learn section

--- src/app/learnBoardStack/page.tsx
+++ src/app/learnBoardStack/page.tsx
@@ -20,9 +20,9 @@ const page = () => {
         </h1>
         <div className="flex flex-col  items-center">
           <h2 className="text-xl md:text-2xl raleway "></h2>
-          <section className="flex   p-5 gap-10 space-x-[200px] fira  mt-20 text-center  ">
+          <section className="flex md:flex-row flex-col  p-5 gap-10 md:space-x-[130px] fira  mt-20 text-center  ">
             {/*video explanation */}
-            <p className=" fira md:text-xl max-w-[600px] self-center text-start bg-white/10 rounded-md shadow-black shadow-md p-2 first-letter:font-bold first-letter:text-[#6ee2f5] font-thin text-pretty leading-relax">
+            <p className=" fira md:text-xl max-w-[600px] min-w-[400px] self-center text-start bg-white/10 rounded-md shadow-black shadow-md p-2 first-letter:font-bold first-letter:text-[#6ee2f5] font-thin text-pretty leading-relax">
               We got too start like everything with the basics.
               <br /> <br />A Kanban board is an agile project management tool
               designed to help visualize work, limit work in progress, and
@@ -40,11 +40,12 @@ const page = () => {
           </section>
         </div>
         <section className="flex flex-col gap-20 mt-20 p-5 items-center justify-center">
-          <h2 className="text-white text-center text-3xl antialiased  fira p-10 bg-[#6ee2f5]/20 ring-[#6ee2f5]/50 shadow-[#6ee2f5]/50 rounded-lg shadow-lg ring-2 ">
+          <h2 className="relative text-center text-3xl antialiased  fira  z-10 text-[#6ee2f5] text-transparent bg-clip-text bg-gradient-to-r from-purple-500 p-2 to-[#6ee2f5] font-bold bg-[#6ee2f5]/20 ">
             What BoardStack offers{" "}
+            <span className=" -z-10 absolute inset-0 ring-[#6ee2f5]/50  shadow-purple-500/50   rounded-lg shadow-lg ring-2 animate-pulse"></span>
           </h2>
           <article className="flex items-center justify-center gap-10">
-            <ol className="list-disc p-2 text-xl fira  ">
+            <ol className="list-disc p-2 md:text-xl text-md fira ">
               <li>
                 <button>Create Cards</button>
               </li>
@@ -52,9 +53,10 @@ const page = () => {
                 <button>Organize Tasks in each card</button>
               </li>
               <li>
-                <button>
-                  Details of task and positibility to add checklists
-                </button>
+                <button>Customizable Details of task</button>
+              </li>
+              <li>
+                <button>Positibility to add checklists</button>
               </li>
               <li>
                 <span className="text-gray-500 ">(currently developing)</span>{" "}
@@ -74,26 +76,38 @@ const page = () => {
               />
             </article>
           </article>
-          <div className="flex flex-col items-center justify-center gap-5">
-            <p className="text-xl fira">
-              Our app includes a login feature with convenient options like
-              Google, or you can register manually. Currently, its designed for
-              managing a single project at a time, as it serves primarily as a
-              portfolio piece.
-            </p>
-            <Image
-              height={300}
-              width={200}
-              src="/assets/blog/kanbanExample.png"
-              alt="img"
-              className="self-center"
-            />
-            <p className="text-xl fira">
-              However, you can save and reload your current project whenever
-              needed. Currently available on the web, we are actively developing
-              the mobile app for Android and iOS platforms.
-            </p>
-          </div>
+          <section className="flex flex-col  items-center justify-center gap-5">
+            <h3 className="z-10  p-2 relative text-center text-3xl antialiased  fira font-bold text-[#6ee2f5] bg-gradient-to-r from-[#6ee2f5]  text-transparent bg-clip-text to-purple-500">
+              What else{" "}
+              <span className=" ring-[#6ee2f5]/50 shadow-[#6ee2f5]/50 rounded-lg shadow-lg ring-2  animate-pulse   -z-10 absolute inset-0 "></span>
+            </h3>
+            <article className="md:flex md:flex-row justify-center flex flex-col items-center bg-gray-500/10 rounded-md shadow-black shadow-md p-10  mt-10 md:flex-wrap">
+              <p className="md:text-xl text-md fira   md:max-w-[600px] mb-5">
+                Our app includes a login feature with convenient options like
+                Google, or you can register manually. Currently, its designed
+                for managing a single project at a time, as it serves primarily
+                as a portfolio piece.
+                <span className="hidden md:block">
+                  <br />
+                  However, you can save and reload your current project whenever
+                  needed. Currently available on the web, we are actively
+                  developing the mobile app for Android and iOS platforms.
+                </span>
+              </p>
+              <Image
+                height={400}
+                width={400}
+                src="/assets/blog/kanbanExample.png"
+                alt="img"
+                className="self-center "
+              />
+              <p className="md:text-xl text-md md:hidden fira md:bg-white/10 rounded-md md:shadow-black md:shadow-md p-2 md:max-w-[600px] mt-5">
+                However, you can save and reload your current project whenever
+                needed. Currently available on the web, we are actively
+                developing the mobile app for Android and iOS platforms.
+              </p>
+            </article>
+          </section>
 
           <aside className="flex  justify-center -mt-5 space-x-5  ">
             <AndroidIcon className="text-5xl text-[#3ddc84]   " />
