38fcef6b4c6435bf8451646ca7732a6f587ec960
gabrielpaparello <69154223+GabrielPaparello@users.noreply.github.com>
2024-07-21 13:23:35 +0000

    x

--- src/app/components/Cards.tsx
+++ src/app/components/Cards.tsx
@@ -9,7 +9,6 @@ import {
   Card,
 } from "../lib/StatesReducers/createCard";
 import ListsAdder from "./ListsAdder";
-import type { DropResult } from 'react-beautiful-dnd';
 import type { DropResult } from "react-beautiful-dnd";
 import { useUser } from "@auth0/nextjs-auth0/client";
 import { v4 as uuid } from "uuid";
