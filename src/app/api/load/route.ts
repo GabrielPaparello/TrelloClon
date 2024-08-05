bbda4408de9b53b807fd69d4c44e9c0743ec3a9c
gabrielpaparello <69154223+GabrielPaparello@users.noreply.github.com>
2024-08-05 13:17:15 +0000

    almost rolling back

--- src/app/api/load/route.ts
+++ src/app/api/load/route.ts
@@ -23,6 +23,50 @@
 //     return NextResponse.json({ message: 'Failed to load data' }, { status: 500 });
 //   }
 // }
+// import { NextRequest, NextResponse } from "next/server";
+// import { db } from "@vercel/postgres";
+
+// export async function GET(req: NextRequest) {
+//   try {
+//     // Extract user_id from request headers
+//     const user_id = req.headers.get("user_id");
+//     const projectId = req.headers.get("projectId");
+//     // Alternatively, you can extract user_id from the body if it's a POST request
+//     // const { user_id } = await req.json();
+
+//     // Log the user_id for debugging purposes
+//     console.log("user_id:", user_id);
+
+//     // Ensure user_id is present and valid
+//     if (!user_id && !projectId) {
+//       return NextResponse.json(
+//         { message: "Missing or invalid user_id, projectId" },
+//         { status: 400 }
+//       );
+//     }
+
+//     const client = await db.connect();
+//     const query =
+//       "SELECT data FROM projects WHERE user_id = $1 AND projectId = $2 LIMIT 1";
+//     const result = await client.query(query, [user_id, projectId]);
+
+//     client.release();
+
+//     if (result.rows.length > 0) {
+//       return NextResponse.json(result.rows[0].data, { status: 200 });
+//     } else {
+//       return NextResponse.json([], { status: 200 });
+//     }
+//   } catch (error) {
+//     console.error("Error loading data:", error);
+//     return NextResponse.json(
+//       { message: "Failed to load data" },
+//       { status: 500 }
+//     );
+//   }
+// }
+
+// OLD DATA load
 import { NextRequest, NextResponse } from "next/server";
 import { db } from "@vercel/postgres";
 
@@ -31,6 +75,7 @@ export async function GET(req: NextRequest) {
     // Extract user_id from request headers
     const user_id = req.headers.get("user_id");
     const projectId = req.headers.get("projectId");
+
     // Alternatively, you can extract user_id from the body if it's a POST request
     // const { user_id } = await req.json();
 
@@ -38,16 +83,16 @@ export async function GET(req: NextRequest) {
     console.log("user_id:", user_id);
 
     // Ensure user_id is present and valid
-    if (!user_id && !projectId) {
+    if (!user_id) {
       return NextResponse.json(
-        { message: "Missing or invalid user_id, projectId" },
+        { message: "Missing or invalid user_id" },
         { status: 400 }
       );
     }
 
     const client = await db.connect();
     const query =
-      "SELECT data FROM projects WHERE user_id = $1 AND projectId = $2 LIMIT 1";
+      "SELECT data FROM projects WHERE user_id = $1 LIMIT 1 AND projectId = $2";
     const result = await client.query(query, [user_id, projectId]);
 
     client.release();
