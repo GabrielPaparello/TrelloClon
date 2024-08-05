2338ec68b49a2246dce5c20b298624b826af7e15
gabrielpaparello <69154223+GabrielPaparello@users.noreply.github.com>
2024-08-05 13:24:49 +0000

    new save?

--- src/app/api/save/route.ts
+++ src/app/api/save/route.ts
@@ -46,15 +46,68 @@
 //   }
 // }
 
+// import { NextRequest, NextResponse } from "next/server";
+// import { db } from "@vercel/postgres";
+
+// export async function POST(req: NextRequest) {
+//   let client;
+//   try {
+//     const { user_id, data, projectId } = await req.json();
+//     console.log(user_id, "iduyser", data, "data", projectId, "projectId");
+//     client = await db.connect();
+
+//     // Check if the record exists
+//     const checkQuery =
+//       "SELECT id FROM projects WHERE user_id = $1 AND projectId = $2";
+//     const checkResult = await client.query(checkQuery, [user_id, projectId]);
+
+//     if (checkResult.rows.length > 0) {
+//       // Update existing record
+//       const updateQuery =
+//         "UPDATE projects SET data = $3 WHERE user_id = $1 AND projectId = $2";
+//       await client.query(updateQuery, [
+//         user_id,
+//         projectId,
+//         JSON.stringify(data),
+//       ]);
+//     } else {
+//       // Insert new record
+//       const insertQuery =
+//         "INSERT INTO projects (user_id, projectId, data) VALUES ($1, $2, $3)";
+//       await client.query(insertQuery, [
+//         user_id,
+//         projectId,
+//         JSON.stringify(data),
+//       ]);
+//     }
+
+//     return NextResponse.json(
+//       { message: "Data saved successfully" },
+//       { status: 200 }
+//     );
+//   } catch (error) {
+//     console.error("Error saving data:", error);
+//     return NextResponse.json(
+//       { message: "Failed to save data" },
+//       { status: 500 }
+//     );
+//   } finally {
+//     if (client) {
+//       client.release();
+//     }
+//   }
+// }
+
+//  OLD SAVA
+
 import { NextRequest, NextResponse } from "next/server";
 import { db } from "@vercel/postgres";
 
 export async function POST(req: NextRequest) {
-  let client;
   try {
     const { user_id, data, projectId } = await req.json();
-    console.log(user_id, "iduyser", data, "data", projectId, "projectId");
-    client = await db.connect();
+
+    const client = await db.connect();
 
     // Check if the record exists
     const checkQuery =
@@ -64,23 +117,24 @@ export async function POST(req: NextRequest) {
     if (checkResult.rows.length > 0) {
       // Update existing record
       const updateQuery =
-        "UPDATE projects SET data = $3 WHERE user_id = $1 AND projectId = $2";
+        "UPDATE projects SET data = $2 WHERE user_id = $1 AND projectId = $3";
       await client.query(updateQuery, [
         user_id,
-        projectId,
         JSON.stringify(data),
+        projectId,
       ]);
     } else {
       // Insert new record
       const insertQuery =
-        "INSERT INTO projects (user_id, projectId, data) VALUES ($1, $2, $3)";
+        "INSERT INTO projects (user_id, data, projectId) VALUES ($1, $2 , $3)";
       await client.query(insertQuery, [
         user_id,
-        projectId,
         JSON.stringify(data),
+        projectId,
       ]);
     }
 
+    client.release();
     return NextResponse.json(
       { message: "Data saved successfully" },
       { status: 200 }
@@ -91,9 +145,5 @@ export async function POST(req: NextRequest) {
       { message: "Failed to save data" },
       { status: 500 }
     );
-  } finally {
-    if (client) {
-      client.release();
-    }
   }
 }
